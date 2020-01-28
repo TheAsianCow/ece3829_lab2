55b49037fe1b687173c0c959708df0dbf60589c0
Jerfy <jeffreyhuangdragon@gmail.com>
2020-01-28 18:28:27 -0500

    fucking display works

--- ece3829_lab2.srcs/sources_1/new/lab2_top.v
+++ ece3829_lab2.srcs/sources_1/new/lab2_top.v
@@ -25,14 +25,23 @@ module lab2_top(
     input clk,
     input reset,
     output [6:0] seg,
-    output [3:0] an
+    output [3:0] an,
+    output Hsync,
+    output Vsync,
+    output reg [3:0] vgaRed,
+    output reg [3:0] vgaBlue,
+    output reg [3:0] vgaGreen
     );
     
     wire clk_out;
+    wire slowclk;
     wire led;
+    wire [10:0] x;
+    wire [10:0] y;
+    wire blank;
     
-    clk_wiz_0 mmcm_inst
-   (
+    
+    clk_wiz_0 mmcm_inst(
     // Clock out ports
     .clk_out1(clk_out),     // output clk_out1
     // Status and control signals
@@ -40,7 +49,34 @@ module lab2_top(
     .locked(led),       // output locked
    // Clock in ports
     .clk_in1(clk));      // input clk_in1
-//    slowclock u0(clk, clk_out);
-    seven_seg u1(sw,clk_out,seg,an);
     
+    //displaying seven seg
+    slowclock u0(clk, slowclk);
+    seven_seg u1(sw,slowclk,seg,an);
+    
+    //vga stuff
+    vga_controller_640_60 display(
+        .rst(reset), 
+        .pixel_clk(clk_out),
+        .HS(Hsync),
+        .VS(Vsync),
+        .hcount(y),
+        .vcount(x),
+        .blank(blank)
+    );
+    always @(*)begin
+    if(blank)
+    begin
+        vgaRed = 4'b0000;
+        vgaBlue = 4'b0000;
+        vgaGreen = 4'b0000;
+    end
+        
+    else 
+    begin
+        vgaRed = 4'b0000;
+        vgaBlue = 4'b1111;
+        vgaGreen = 4'b0000;
+    end
+    end
 endmodule
