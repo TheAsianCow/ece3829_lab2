c97e0efcf276c8107a0658fd03844071f0a338ea
Alyssa Ungerer <alyssaungerer@gmail.com>
2020-01-28 14:49:35 -0500

    25 MHz clk first draft

--- ece3829_lab2.srcs/sources_1/new/mmcm.v
+++ ece3829_lab2.srcs/sources_1/new/mmcm.v
@@ -23,26 +23,32 @@
 module mmcm(
     input clk_fpga,
     input reset,
-    output reg clk_slow,
-    output locked
+    output output_lock
     );
     
     reg [15:0] count;
     wire clk_25M;
     
+   clk_wiz_0 mmcm_inst
+   (
+    // Clock out ports
+    .clk_out1(clk_fpga),     // output clk_out1
+    // Status and control signals
+    .reset(reset), // input reset
+    .locked(output_lock),       // output locked
+   // Clock in ports
+    .clk_in1(clk_25M));      // input clk_in1
     
     always @ (posedge clk_fpga, posedge reset)
     begin
         if (reset)
             begin
             count <= 1'b0;
-            clk_slow <= 16'b0;
             end
         else
             if (count == 4'h4)
                 begin
                 count <= 4'b0;
-                clk_slow <= clk_slow + 1;
                 end
             else
                 count <= count + 1'b1;
