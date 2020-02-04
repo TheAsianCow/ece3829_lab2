7a7b4eafee4a998dc34af4923082705d6e497d79
Jerfy <jeffreyhuangdragon@gmail.com>
2020-02-04 16:11:42 -0500

    added data capture flag

--- ece3829_lab2.srcs/sources_1/new/shift_register.v
+++ ece3829_lab2.srcs/sources_1/new/shift_register.v
@@ -30,9 +30,11 @@ module shift_register(
     
     reg [14:0] data = 15'b0;
     reg [3:0] cnt = 4'b0000;
+    reg cap_flag = 1'b0;
     
     always @ (posedge clk)begin
-//        if(data_cap)begin
+        if(data_cap) cap_flag = 1'b1;
+        if(cap_flag)begin
             if(data[14:12] == 3'b0 && data[3:0] == 4'b0) lightdata = data [11:4];
             else data <= {data[13:0] , sdo};
             if(cnt<15)begin
@@ -42,7 +44,8 @@ module shift_register(
             else begin
                 cs <= 1'b1;
                 cnt <= 1'b0;
+                cap_flag = 1'b0;
             end
-//        end
+        end
     end
 endmodule
